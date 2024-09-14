e8964dbf64f5fb77f897cc7cb57f77f69a42f5ef
Conor Le <lethanhvu2004@gmail.com>
2024-09-13 22:30:55 -0400

    Lab 2

--- Lab/Lab2.js
+++ Lab/Lab2.js
@@ -1,4 +1,5 @@
 // Ex1
+console.log('Excercise 1');
 const greeter = (myArray, counter) => {
   const greetText = "Hello";
 
@@ -10,23 +11,27 @@ const greeter = (myArray, counter) => {
 greeter(["Randy Savage", "Ric Flair", "Hulk Hogan"], 3);
 
 // Ex2
+console.log('\nExcercise 2');
 const capitalizeFirstLetter = ([first, ...rest]) => first.toUpperCase() + rest.join('');
 
 console.log(capitalizeFirstLetter("hello")); // Output: "Hello"
 
 // Ex3
+console.log('\nExcercise 3');
 const colors = ["red", "green", "blue"];
 const capitalizeColors = colors.map(color => capitalizeFirstLetter(color));
 
 console.log(capitalizeColors); // Output: ["Red", "Green", "Blue"]
 
 // Ex4
+console.log('\nExcercise 4');
 const numbers = [10, 15, 25, 30, 5, 20];
 const filteredNumbers = numbers.filter(num => num >= 20);
 
 console.log(filteredNumbers); // Output: [25, 30, 20]
 
 // Ex5
+console.log('\nExcercise 5');
 const nums = [1, 2, 3, 4];
 const sum = nums.reduce((total, num) => total + num, 0);
 const product = nums.reduce((total, num) => total * num, 1);
@@ -35,6 +40,7 @@ console.log(`Sum: ${sum}`); // Output: "Sum: 10"
 console.log(`Product: ${product}`); // Output: "Product: 24"
 
 // Ex6
+console.log('\nExcercise 6');
 class Car {
   constructor(model, year) {
     this.model = model;
