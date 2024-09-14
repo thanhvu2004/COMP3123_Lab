8fb6b4d083f1f0d12e28190fd0467c8f2c73c66b
Conor Le <lethanhvu2004@gmail.com>
2024-09-13 21:56:44 -0400

    Lab2 work

---
+++ Lab1/Lab2.js
@@ -0,0 +1,53 @@
+// Ex1
+const greeter = (myArray, counter) => {
+  const greetText = "Hello";
+
+  for (const element of myArray) {
+    console.log(`${greetText} ${element}`);
+  }
+};
+
+greeter(["Randy Savage", "Ric Flair", "Hulk Hogan"], 3);
+
+// Ex2
+const capitalizeFirstLetter = ([first, ...rest]) => first.toUpperCase() + rest.join('');
+
+console.log(capitalizeFirstLetter("hello")); // Output: "Hello"
+
+// Ex3
+const colors = ["red", "green", "blue"];
+const capitalizeColors = colors.map(color => capitalizeFirstLetter(color));
+
+console.log(capitalizeColors); // Output: ["Red", "Green", "Blue"]
+
+// Ex4
+const numbers = [10, 15, 25, 30, 5, 20];
+const filteredNumbers = numbers.filter(num => num >= 20);
+
+console.log(filteredNumbers); // Output: [25, 30, 20]
+
+// Ex5
+const nums = [1, 2, 3, 4];
+const sum = nums.reduce((total, num) => total + num, 0);
+const product = nums.reduce((total, num) => total * num, 1);
+
+console.log(`Sum: ${sum}`); // Output: "Sum: 10"
+console.log(`Product: ${product}`); // Output: "Product: 24"
+
+// Ex6
+class Car {
+  constructor(model, year) {
+    this.model = model;
+    this.year = year;
+  }
+}
+
+class Sedan extends Car {
+  constructor(model, year, balance) {
+    super(model, year);
+    this.balance = balance;
+  }
+}
+
+const mySedan = new Sedan("Toyota", 2020, 15000);
+console.log(mySedan); // Output: Sedan { model: 'Toyota', year: 2020, balance: 15000 }
