704afe557a7579865c4eaece6d62478844ba0d2c
Conor Le <lethanhvu2004@gmail.com>
2024-10-12 16:39:58 -0400

    Initial set up

---
+++ 101411302_COMP3123-assignment01/models/UsersModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the User schema
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Hash the password before saving the user
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+        } catch (err) {
+            return next(err);
+        }
+    }
+    next();
+});
+
+// Create the User model
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
